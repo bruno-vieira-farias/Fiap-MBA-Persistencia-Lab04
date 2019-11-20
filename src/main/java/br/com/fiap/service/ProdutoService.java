335a51659bbfe41d0095b710aeac372f879640f5
Bruno Farias <contato.brunofarias@gmail.com>
2019-11-19 21:45:36 -0300

    Carga inicial.

---
+++ src/main/java/br/com/fiap/service/ProdutoService.java
@@ -0,0 +1,36 @@
+package br.com.fiap.service;
+
+import br.com.fiap.model.Produto;
+import br.com.fiap.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.Collection;
+import java.util.List;
+
+@Component
+public class ProdutoService {
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    @Transactional
+    public void add(Produto produto) {
+        produtoRepository.save(produto);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Produto> findAll() {
+        return produtoRepository.findAll();
+    }
+
+    @Transactional
+    public void addAll(Collection<Produto> produtos) {
+        for (Produto produto : produtos) {
+            produtoRepository.save(produto);
+        }
+    }
+
+
+}
